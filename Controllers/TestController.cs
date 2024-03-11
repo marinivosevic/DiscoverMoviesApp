a2c974a5b0b6c42c458923cec7e57d56720e905c
marinivosevic <95760234+marinivosevic@users.noreply.github.com>
2024-03-11 11:50:07 +0100

    Fixed response error

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -50,17 +50,23 @@ namespace ExpensesTracker.Controllers
                 if (response.IsSuccessful)
                 {
                     var responseData = response.Content != null ? JsonConvert.DeserializeObject<ApiResponse>(response.Content) : null;
-
+                  
                     if (responseData?.Results != null)
                     {
                         var resultID = responseData.Results[0].Id;
-                        var similarMovies = await GetSimilarMovies(resultID);
+                        var optionsSimilarMovies = new RestClientOptions("https://api.themoviedb.org/3/movie/293660/similar?language=en-US&page=1");
+                        var clientSimilarMovies = new RestClient(optionsSimilarMovies);
+                        var requestSimilarMovies = new RestRequest("");
+                        requestSimilarMovies.AddHeader("accept", "application/json");
+                        requestSimilarMovies.AddHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjcyNzNjYWNiMzZiZWI0NmU1YzMxNjJmNmUwNDY0MyIsInN1YiI6IjYzZmZiZGMyOWYxYmU3MDA3Y2E2YmM3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C53rNh4eQFNwLBjoRy0Yilk6e_NwDaAmOivcyTI4p-w");
+                        requestSimilarMovies.AddParameter("query", resultID);
+                        var responseSimilarMovies = await clientSimilarMovies.GetAsync(requestSimilarMovies);
 
-                        var parsedData = JsonConvert.DeserializeObject<MovieData>(similarMovies.ToString());
+                        //var parsedData = JsonConvert.DeserializeObject<MovieData>(similarMovies.ToString());
 
-                        return Ok(similarMovies);
+                        return Ok(responseSimilarMovies.Content);
                     }
-
+ 
                     return NotFound("No movie found");
                 }
                 else
@@ -76,32 +82,7 @@ namespace ExpensesTracker.Controllers
 
 
         }
-        [HttpGet]
-        private async Task<IActionResult> GetSimilarMovies(int MovieId)
-        {
-
-            var options = new RestClientOptions("https://api.themoviedb.org/3/movie/293660/similar?language=en-US&page=1");
-            var client = new RestClient(options);
-            var request = new RestRequest("");
-            request.AddHeader("accept", "application/json");
-            request.AddHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjcyNzNjYWNiMzZiZWI0NmU1YzMxNjJmNmUwNDY0MyIsInN1YiI6IjYzZmZiZGMyOWYxYmU3MDA3Y2E2YmM3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C53rNh4eQFNwLBjoRy0Yilk6e_NwDaAmOivcyTI4p-w");
-            request.AddParameter("query", MovieId);
-            var response = await client.GetAsync(request);
-
-            return Ok(response.Content);
-        }
-
-        public class MovieData
-        {
-            public MovieContainer Value { get; set; }
-        }
-
-        public class MovieContainer
-        {
-            public int Page { get; set; }
-            public Movie[] Results { get; set; }
-        }
-
+       
         public class Movie
         {
             // Define properties according to your JSON structure
