0d2d9365e3201fe1384d39b9ca787e559e2c3bf2
marinivosevic <95760234+marinivosevic@users.noreply.github.com>
2024-07-29 18:49:16 +0200

    Update axios.js

--- Backend/ClientApp/src/api/config/axios.js
+++ Backend/ClientApp/src/api/config/axios.js
@@ -4,7 +4,7 @@ const headers = {
     "Content-Type": "application/json",
 }
 
-const baseURL = "http://findyourmovieapi.us-east-1.elasticbeanstalk.com/api/";
+const baseURL = "http://findmymovie.eu-central-1.elasticbeanstalk.com/api/";
 
 export const axiosInstance = axios.create({
     baseURL,
